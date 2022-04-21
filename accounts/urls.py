12d79683ec14a4f4980d79d46bc384c58aea2102
Zach <zachattack6089@gmail.com>
2022-04-21 15:19:36 -0700

    Reverted to basic setup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from . import views 
-from .views import SignUpView
+from . import views
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path("", views.index, name="index"),
 ]
--- vendors/views.py
+++ vendors/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("products index test")
+    return HttpResponse("vendors index test")
