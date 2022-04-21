82dbcb171c6a988c50ebbe6a2005b113fb9319be
Zach <zachattack6089@gmail.com>
2022-04-21 15:23:57 -0700

    Removed templates and added views for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("accounts index test")
+
+def signup(request):
+    return HttpResponse("accounts signup test")
+
+def login(request):
+    return HttpResponse("accounts login test")
+
+def logout(request):
+    return HttpResponse("accounts logout test")
