8d361d26a461baf648edb15d0884749f8527e0bb
WoodardEric <woodard.eric09@gmail.com>
2022-04-21 20:08:09 -0700

    add product page

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
+from enum import Enum, IntEnum
+from pickle import FALSE
 from django.db import models
 
-# Create your models here.
+class ProductType(models.TextChoices):
+    PHONE = 'phone'
+    TV = 'tv' 
+
+class Product(models.Model):
+    type = models.CharField(max_length=200, choices=[(tag, tag.value) for tag in ProductType], null=False)
+    name = models.CharField(max_length=200, null=False)
+    brand = models.CharField(max_length=200, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+
+    def __str__(self):
+        return str(self.name) + ": $" + str(self.price)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,27 @@
+from msilib.schema import Class
+from unittest import result
+from django.utils import timezone
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Product
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 50
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+def index(request):
+    if request.method == "POST":
+        query_name = request.POST.get('name', None)
+        results = Product.objects.filter(name__contains=query_name)
+        return render(request, 'products.html', {"products":results})
+    else:
+        context = {}
+        context["products"] = Product.objects.all(); 
+        return render(request, 'products.html', context)
+
+
