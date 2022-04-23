55ef5ddafc424423982926716dfd1839d7edaffa
Zach <zachattack6089@gmail.com>
2022-04-22 20:51:20 -0700

    Fixed URL for products/cart/, added header to signup and login pages

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("cart", views.cart, name='cart'),
+    path("cart/", views.cart, name='cart'),
     path("add_to_cart", views.add_to_cart, name='add_to_cart'),
     path("", views.index, name='index'),
 ]
