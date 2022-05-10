570b26e7a84fff6940f8b5ea69de74680f3ef760
WoodyEric <woodard.eric09@gmail.com>
2022-05-10 13:28:03 -0700

    add simple about page

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html')
