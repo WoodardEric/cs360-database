49fc57947022ffb1713086a297b6844baa22ccc4
zp100 <60728715+zp100@users.noreply.github.com>
2022-05-10 22:17:41 -0700

    Added static root

--- BestBuy/settings.py
+++ BestBuy/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = (
     BASE_DIR / "static",
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
